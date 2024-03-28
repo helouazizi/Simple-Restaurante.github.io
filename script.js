0f292ab2d176c85406bd90d85605ee4a67e23156
ouazizi <ouazizi2028@gmail.com>
2024-03-28 02:41:23 +0000

    somme javascript to menu page

---
+++ script.js
@@ -0,0 +1,25 @@
+// lets get acces the navbar links
+let home = document.querySelector('.home');
+let menu = document.querySelector('.menu');
+let about = document.querySelector('.about');
+let contact = document.querySelector('.contact');
+let contentheader = document.querySelector('.content-header');
+
+// lets get acces the card element
+let card = document.querySelector('.card');
+let foodimg = document.querySelector('.foodimg');
+let foodname = document.querySelector('.foodname');
+let foodprice = document.querySelector('.foodprice');
+let fooddescription = document.querySelector('.fooddescription');
+let foodordre = document.querySelector('.foodordre');
+
+
+// =========== lets strat coding the menu page inside the home page ========//
+
+menu.addEventListener('click',function(){
+    menu.classList.add('active');
+    home.classList.remove('active');
+    contentheader.textContent = 'Our menu';
+    contentheader.style.textDecorationLine = 'underline ';
+    
+})
